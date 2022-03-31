93b1f3b11ae88518af9d41eb861a15f1a291db08
Jimmyhee <nepuhzm@163.com>
2022-03-31 16:31:45 +0800

    feat:创建单例

---
+++ src/main/java/com/hzm/mode/SingleTonMode.java
@@ -0,0 +1,17 @@
+package com.hzm.mode;/**
+ * @author xxx
+ * @e-mail xxx
+ * @date 2022/3/31 16:31
+ * @package com.hzm.mode
+ * @description
+ */
+
+/**
+ * @program: ModeDes
+ *
+ * @author: Jimmy.He
+ *
+ * @create: 2022-03-31 16:31
+ **/
+public class SingleTonMode {
+}
